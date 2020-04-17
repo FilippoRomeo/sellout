9454101885ab8f85d5e4ff1cdd803775d32f8d90
Filippo <romeofilippo95@gmail.com>
2020-04-17 16:51:11 +0100

    getads and user.blade back to normal

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -43,35 +43,7 @@
                     </div>
                     @endif
                 </div>
-                <div class="row" id="Advertisements">
-                <div class="container">
-                        <div class="row">
-                            @if(count($product)>0)
-                            @foreach($product as $row)
-                            <div class="col-md-3">
-                                <div class="productCard" style="text-align:center; margin:1rem width: 20rem;">
-                                    <img src=<?php echo strtok($row->photos, ',') ?> style=" width:100%; height:182px;" />
-                                    <div class="card-header" style="border:1px solid #ccc !important;">
-                                        <h4 style="margin-bottom: 0px; text-align:center;">{{$row->productName}}</h4>
-                                    </div>
-
-                                    <div class="card-body" style="border:1px solid #ccc !important;">
-                                        <h4 style="margin-bottom: 0px; text-align:center;">£{{$row->expSellPrice}}</h4>
-                                        <h4 style="margin-bottom: 0px; text-align:center;">{{$row->city}}</h4>
-                                        <div class="card border-secondary" style="max-width: 10rem; border:1px solid #ccc !important; margin:auto; padding:0.4rem;">
-                                            <p style="margin-bottom: 0px; text-align:center;">
-                                                <a href='{{url("/product/view/{$row->id}")}}' {{$row->productName}}>View</a></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            @endforeach
-                            @else
-                            <p>No element for this category...</p>
-                            @endif
-                        </div>
-                    </div>
-                </div>
+                <div class="row" id="Advertisements"></div>
             </div>
         </div>
     </div>
